3acaa0fe3e74dc4ebba869e537d06611f58397c8
sakots <sakots8000@gmail.com>
2021-10-26 22:51:32 +0900

    theme basic

---
+++ sfup/templates/basic/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{$title}}</title>
+    <link rel="stylesheet" href="templates/{{$themedir}}/scss/base.min.css">
+</head>
+
+<body>
+<header>
+    <div>
+        <h1>{{$title}}</h1>
+    </div>
+</header>
+<main>
+    <section id="up">
+        <h2>ろだ</h2>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <div class="upload-area">
+                <div class="dropbox">
+                    <svg viewBox="0 0 640 512"><use href="templates/{{$themedir}}/icons/cloud-upload-alt.svg#cloud-upload"></svg>
+                </div>
+                <p>Drag and drop a file or click</p>
+                <input type="file" name="upfile[]" id="input-files" multiple>
+            </div>
+            <div>
+                <input type="text" placeholder="あいことば" autocomplete="current-password">
+                <input type="submit" id="submit-btn" value=" うp ">
+            </div>
+        </form>
+    </section>
+    <section id="files">
+        <h2>ファイル</h2>
+        <div>
+
+        </div>
+    </section>
+</main>
+<footer>
+    <!-- 著作権表示 -->
+    <div class="copy">
+        <p>
+            SELF UPLOADER {{$ver}} &copy; 2021 sakots
+            <a href="https://github.com/sakots/self_uploader" class="github"><svg viewBox="0 0 496 512"><use href="templates/{{$themedir}}/icons/github.svg#github"></svg>github</a>
+        </p>
+        <p>
+            theme - {{$t_name}} {{$t_ver}} by sakots
+        </p>
+    </div>
+</footer>
+</body>
+</html>
---
+++ sfup/templates/basic/theme.ini.php
@@ -0,0 +1,30 @@
+<?php
+//--------------------------------------------------
+//  SELF UPLOADER v0.0.1～用テーマ「basic」設定ファイル
+//  by sakots https://dev.oekakibbs.net/
+//--------------------------------------------------
+
+//テーマ名
+define('THEME_NAME', "basic");
+
+//テーマのバージョン
+define('THEME_VER', "v0.0.0 lot.211026.0");
+
+/* -------------------- */
+
+//テーマがXHTMLか 1:XTHML 0:HTML
+define('TH_XHTML', 0);
+
+/* -------------------- */
+
+//テンプレートファイル
+/* ファイル名に".blade.php"は不要 */
+
+//メイン
+define('MAINFILE', "index");
+
+//管理モード
+define('ADMINFILE', "admin");
+
+//その他
+define('OTHERFILE', "other");
