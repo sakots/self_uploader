d146c1a09cc27106fd092448a0e8f9e3f217d22a
sakots <sakots8000@gmail.com>
2025-07-14 13:38:57 +0900

    tab

--- sfup/templates/basic/js/upload.js
+++ sfup/templates/basic/js/upload.js
@@ -1,46 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const dropbox = document.querySelector('.dropbox');
-    const inputFiles = document.getElementById('input-files');
-    const uploadArea = document.querySelector('.upload-area');
+  const dropbox = document.querySelector('.dropbox');
+  const inputFiles = document.getElementById('input-files');
+  const uploadArea = document.querySelector('.upload-area');
 
-    // ドラッグオーバー時のイベント
-    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-        uploadArea.addEventListener(eventName, preventDefaults, false);
-    });
+  // ドラッグオーバー時のイベント
+  ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    uploadArea.addEventListener(eventName, preventDefaults, false);
+  });
 
-    function preventDefaults(e) {
-        e.preventDefault();
-        e.stopPropagation();
-    }
+  function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
 
-    // ドラッグオーバー時のスタイル変更
-    ['dragenter', 'dragover'].forEach(eventName => {
-        uploadArea.addEventListener(eventName, highlight, false);
-    });
+  // ドラッグオーバー時のスタイル変更
+  ['dragenter', 'dragover'].forEach(eventName => {
+    uploadArea.addEventListener(eventName, highlight, false);
+  });
 
-    ['dragleave', 'drop'].forEach(eventName => {
-        uploadArea.addEventListener(eventName, unhighlight, false);
-    });
+  ['dragleave', 'drop'].forEach(eventName => {
+    uploadArea.addEventListener(eventName, unhighlight, false);
+  });
 
-    function highlight(e) {
-        uploadArea.classList.add('highlight');
-    }
+  function highlight(e) {
+    uploadArea.classList.add('highlight');
+  }
 
-    function unhighlight(e) {
-        uploadArea.classList.remove('highlight');
-    }
+  function unhighlight(e) {
+    uploadArea.classList.remove('highlight');
+  }
 
-    // ファイルドロップ時の処理
-    uploadArea.addEventListener('drop', handleDrop, false);
+  // ファイルドロップ時の処理
+  uploadArea.addEventListener('drop', handleDrop, false);
 
-    function handleDrop(e) {
-        const dt = e.dataTransfer;
-        const files = dt.files;
-        inputFiles.files = files;
-    }
+  function handleDrop(e) {
+    const dt = e.dataTransfer;
+    const files = dt.files;
+    inputFiles.files = files;
+  }
 
-    // クリックでファイル選択
-    dropbox.addEventListener('click', () => {
-        inputFiles.click();
-    });
+  // クリックでファイル選択
+  dropbox.addEventListener('click', () => {
+    inputFiles.click();
+  });
 }); 
